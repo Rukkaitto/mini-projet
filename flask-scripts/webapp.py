ae4ce142b15d2f0aff91f6252e3a1620fa7cc3e4
Lucas Goudin <theprocraft@gmail.com>
2018-04-23 17:12:07 +0200

    Added drop down menus

--- flask-scripts/webapp.py
+++ flask-scripts/webapp.py
@@ -1,10 +1,9 @@
-# coding=utf-8
-
-import time
-from flask import *
-import sys
-import random as r
+from flask import Flask, render_template, request
 import psycopg2
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
     
 #NE PAS MODIFIER LA LIGNE SUIVANTE
 app = Flask(__name__)
@@ -20,14 +19,14 @@ def liste_regions():
     page = '<h2>Liste des regions Francaises :</h2>'
     regions = query_db_local(DATABASE, 'select nom from regions')
     #regions = query_db('dbserver', 'bpinaud', 'lgoudin', 'select nom from regions')
-    print(regions)
     for i in regions:
         page = page + i[0] + '<br>'
     return page
 
 @app.route("/liste_departements_region_request")
 def liste_departements_region_request():
-    return app.send_static_file("liste_departements_region_form.html")
+    regions = query_db_local(DATABASE, 'select * from regions')
+    return render_template('liste_departements_region_form.html', regions=regions)
     
 @app.route("/liste_departements_region", methods=['POST'])
 def liste_departements_region():
@@ -41,44 +40,68 @@ def liste_departements_region():
 
 @app.route("/liste_departements_annee_diplome_request")
 def liste_departements_annee_diplome_request():
-    return app.send_static_file("liste_departements_annee_diplome_form.html")
+    annees = query_db_local(DATABASE, 'select distinct annee from nb_diplomes_dept order by annee asc')
+    diplomes = query_db_local(DATABASE, 'select * from niveaux_diplomes')
+    return render_template("liste_departements_annee_diplome_form.html", annees=annees, diplomes=diplomes)
 
 @app.route("/liste_departements_annee_diplome", methods=['POST'])
 def liste_departements_annee_diplome():
     page=""
+    annee = request.form["annee"]
+    niveau = request.form["diplome"]
+    depts = query_db_local(DATABASE, 'select distinct nom from departements dp, nb_diplomes_dept nb where dp.num=nb.dept and nb.annee={} and nb.niveau={} and (select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'H\')>(select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'F\')'.format(annee,niveau,annee,niveau,annee,niveau))
+    
+    if depts:
+        for dept in depts:
+            page += dept[0] + '<br>'
+    else:
+        page='Il n\'existe aucun departement dans lequel il y a plus de femmes diplomees que d\'hommes a cette annee et a ce niveau de diplome.'
     return page
 
-def get_db_local(db_name):
-    print("Connecting to database {}...".format(db_name))
-    db = psycopg2.connect(dbname=db_name)
-    return db
+##########################################################################################
+
+def get_db_local(dbname):
+    try:
+        print("Trying to connect to database {}...".format(dbname))
+        db = psycopg2.connect(dbname=dbname)
+        print("Connected to database {}!".format(dbname))
+        return db
+    except Exception as e:
+        return "Error when connecting to {}: {}".format(dbname, str(e))
 
 def query_db_local(database, command):
     cur = get_db_local(database).cursor()
-    print("Executing command {} on database {}...".format(command, database))
-    cur.execute(command)
-    rv = cur.fetchall()
-    cur.close()
-    return rv
+    print('Trying to execute command "{}" on database {}...'.format(command, database))
+    try:
+        cur.execute(command)
+        rv = cur.fetchall()
+        cur.close()
+        print("Command executed successfully!")
+        return rv
+    except Exception as e:
+        return 'Error when running command "{}": {}'.format(command, str(e))
+
 
 def get_db(host, dbname, user):
     print("Trying to connect to database {} with user {} and host {}...".format(dbname, user, host))
     try:
         db = psycopg2.connect(host=host, dbname=dbname, user=user)
+        print("Connected to database {}!".format(dbname))
         return db
     except Exception as e:
         return "Error when connecting to {}: {}".format(dbname, str(e))
 
 def query_db(host, dbname, user, command):
     cur = get_db(host, dbname, user)
-    print("Trying to execute command: {}; on database {} with user {} and host {}...".format(command, dbname, user, host))
+    print('Trying to execute command "{}" on database {} with user {} and host {}...'.format(command, dbname, user, host))
     try:
         cur.execute(command)
         rv = cur.fetchall()
         cur.close()
+        print("Command executed successfully!")
         return rv
     except Exception as e:
-        return "Error when running command: {}: {}".format(command, str(e))
+        return 'Error when running command "{}": {}'.format(command, str(e))
 
     
 
