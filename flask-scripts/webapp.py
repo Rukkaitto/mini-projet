1b6c79ec3726aab02b57ccc0294c03b2c214de49
Lucas Goudin <lgoudin@emi.u-bordeaux1.fr>
2018-04-24 13:50:28 +0200

    Changed some stuff at CREMI

--- flask-scripts/webapp.py
+++ flask-scripts/webapp.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, request
 import psycopg2
-import sys  
-
-reload(sys)  
-sys.setdefaultencoding('utf8')
     
 #NE PAS MODIFIER LA LIGNE SUIVANTE
 app = Flask(__name__)
@@ -19,28 +15,29 @@ def hello():
 
 @app.route("/liste_regions", methods=['GET'])
 def liste_regions():
-    regions = query_db_local(DATABASE, 'select nom from regions order by nom asc')
-    #regions = query_db(HOST, DATABASE_SERVER, USER, 'select nom from regions order by nom asc')
+    #regions = query_db_local(DATABASE, 'select nom from regions order by nom asc')
+    regions = query_db(HOST, DATABASE_SERVER, USER, 'select nom from regions order by nom asc')
     return render_template('liste_regions.html', regions=regions)
 
 @app.route("/liste_departements_region_request")
 def liste_departements_region_request():
-    regions = query_db_local(DATABASE, 'select * from regions order by nom asc')
-    #regions = query_db(HOST, DATABASE_SERVER, USER, 'select * from regions order by nom asc')
+    #regions = query_db_local(DATABASE, 'select * from regions order by nom asc')
+    regions = query_db(HOST, DATABASE_SERVER, USER, 'select * from regions order by nom asc')
     return render_template('liste_departements_region_form.html', regions=regions)
     
 @app.route("/liste_departements_region", methods=['POST'])
 def liste_departements_region():
     region = request.form["region"]
-    departements = query_db_local(DATABASE, 'select d.nom, d.num from regions r, departements d where r.num=d.region and r.nom=\'{}\' order by d.nom asc'.format(region))
-    #departements = query_db('dbserver', 'bpinaud', 'lgoudin', 'select d.nom, d.num from regions r, departements d where r.num=d.region and r.nom=\'{}\' order by d.nom asc'.format(region))
+    #departements = query_db_local(DATABASE, 'select d.nom, d.num from regions r, departements d where r.num=d.region and r.nom=\'{}\' order by d.nom asc'.format(region))
+    departements = query_db('dbserver', 'bpinaud', 'lgoudin', 'select d.nom, d.num from regions r, departements d where r.num=d.region and r.nom=\'{}\' order by d.nom asc'.format(region))
     return render_template('liste_departements.html', depts=departements, region=region)
 
 @app.route("/liste_departements_annee_diplome_request")
 def liste_departements_annee_diplome_request():
-    annees = query_db_local(DATABASE, 'select distinct annee from nb_diplomes_dept order by annee asc')
-    diplomes = query_db_local(DATABASE, 'select * from niveaux_diplomes')
-    #diplomes = query_db(HOST, DATABASE_SERVER, USER, 'select * from niveaux_diplomes')
+    #annees = query_db_local(DATABASE, 'select distinct annee from nb_diplomes_dept order by annee asc')
+    annees = query_db(HOST, DATABASE_SERVER, USER, 'select distinct annee from nb_diplomes_dept order by annee asc')
+    #diplomes = query_db_local(DATABASE, 'select * from niveaux_diplomes')
+    diplomes = query_db(HOST, DATABASE_SERVER, USER, 'select * from niveaux_diplomes') 
     return render_template("liste_departements_annee_diplome_form.html", annees=annees, diplomes=diplomes)
 
 @app.route("/liste_departements_annee_diplome", methods=['POST'])
@@ -48,8 +45,8 @@ def liste_departements_annee_diplome():
     page=""
     annee = request.form["annee"]
     niveau = request.form["diplome"]
-    depts = query_db_local(DATABASE, 'select distinct nom from departements dp, nb_diplomes_dept nb where dp.num=nb.dept and nb.annee={} and nb.niveau={} and (select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'H\')>(select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'F\')'.format(annee,niveau,annee,niveau,annee,niveau))
-    #depts = query_db(HOST, DATABASE_SERVER, USER, 'select distinct nom from departements dp, nb_diplomes_dept nb where dp.num=nb.dept and nb.annee={} and nb.niveau={} and (select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'H\')>(select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'F\')'.format(annee,niveau,annee,niveau,annee,niveau))
+    #depts = query_db_local(DATABASE, 'select distinct nom from departements dp, nb_diplomes_dept nb where dp.num=nb.dept and nb.annee={} and nb.niveau={} and (select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'H\')>(select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'F\')'.format(annee,niveau,annee,niveau,annee,niveau))
+    depts = query_db(HOST, DATABASE_SERVER, USER, 'select distinct nom from departements dp, nb_diplomes_dept nb where dp.num=nb.dept and nb.annee={} and nb.niveau={} and (select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'H\')>(select count(*) from nb_diplomes_dept where annee={} and niveau={} and sexe=\'F\')'.format(annee,niveau,annee,niveau,annee,niveau))
     
     if depts:
         for dept in depts:
@@ -67,7 +64,7 @@ def get_db_local(dbname):
         print("Connected to database {}!".format(dbname))
         return db
     except Exception as e:
-        return "Error when connecting to {}: {}".format(dbname, str(e))
+        print('Error when connecting to {}: {}'.format(dbname, str(e)))
 
 def query_db_local(database, command):
     cur = get_db_local(database).cursor()
@@ -79,7 +76,7 @@ def query_db_local(database, command):
         print("Command executed successfully!")
         return rv
     except Exception as e:
-        return 'Error when running command "{}": {}'.format(command, str(e))
+        print('Error when running command "{}": {}'.format(command, str(e)))
 
 
 def get_db(host, dbname, user):
@@ -89,10 +86,10 @@ def get_db(host, dbname, user):
         print("Connected to database {}!".format(dbname))
         return db
     except Exception as e:
-        return "Error when connecting to {}: {}".format(dbname, str(e))
+        print("Error when connecting to {}: {}".format(dbname, str(e)))
 
 def query_db(host, dbname, user, command):
-    cur = get_db(host, dbname, user)
+    cur = get_db(host, dbname, user).cursor()
     print('Trying to execute command "{}" on database {} with user {} and host {}...'.format(command, dbname, user, host))
     try:
         cur.execute(command)
@@ -101,7 +98,7 @@ def query_db(host, dbname, user, command):
         print("Command executed successfully!")
         return rv
     except Exception as e:
-        return 'Error when running command "{}": {}'.format(command, str(e))
+        print('Error when running command "{}": {}'.format(command, str(e)))
 
     
 
